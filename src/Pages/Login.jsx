6c4b7175acd5ac379b97098b133384e2bc19e5cc
haroon909 <aharoon909@gmail.com>
2024-09-20 12:19:14 +0500

    --- Auto Git Commit ---

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 
 const Login = () => {
-  const [userLogin, setUserLogin] = useState([]);
+//   const [userLogin, setUserLogin] = useState([]);
   const [userEmail, setUserEmail] = useState("");
   const [UserPassword, setUserPassword] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const 
+    const loginUser ={
+        loginemail: userEmail,
+        loginpassword: UserPassword,
+    }
     const response = await fetch(
         "https://66d806e137b1cadd8053106b.mockapi.io/Users"
       );
@@ -27,12 +30,12 @@ const Login = () => {
       <form onSubmit={handleSubmit}>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"  onChange={(e) => userEmail(e.target.value)}/>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"  onChange={(e) => setUserEmail(e.target.value)}/>
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1"  onChange={(e) => UserPassword(e.target.value)}/>
+    <input type="password" class="form-control" id="exampleInputPassword1"  onChange={(e) => setUserPassword(e.target.value)}/>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
