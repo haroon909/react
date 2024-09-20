06b96348540312d4f36289880c707ef717817389
haroon909 <aharoon909@gmail.com>
2024-09-20 12:17:29 +0500

    --- Auto Git Commit ---

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 
 const Login = () => {
   const [userLogin, setUserLogin] = useState([]);
-
+  const [userEmail, setUserEmail] = useState("");
+  const [UserPassword, setUserPassword] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const 
     const response = await fetch(
         "https://66d806e137b1cadd8053106b.mockapi.io/Users"
       );
@@ -15,11 +17,9 @@ const Login = () => {
 
 }
 
-  useEffect(() => {
-    
-
-    fetchUserLogin();
-  }, []);
+//   useEffect(() => {
+//     fetchUserLogin();
+//   }, []);
 
  
   return (
@@ -27,12 +27,12 @@ const Login = () => {
       <form onSubmit={handleSubmit}>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"  onChange={(e) => LoginEmail(e.target.value)}/>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"  onChange={(e) => userEmail(e.target.value)}/>
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1"  onChange={(e) => LoginPassword(e.target.value)}/>
+    <input type="password" class="form-control" id="exampleInputPassword1"  onChange={(e) => UserPassword(e.target.value)}/>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
